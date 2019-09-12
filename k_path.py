c5fee0b6e1770ffe824ed722a4fd37d8fbea76e0
Adrian Popescu <elementh11@gmail.com>
2019-09-12 10:31:11 -0400

    update k_path.py

--- k_path.py
+++ k_path.py
@@ -2,7 +2,7 @@
 
 This script allows the user to generate the k-path in the BZ for bandstructure calculations
 
-This script requires as input the k_points dictionary containing:
+This script requires as input the k_points dictionary containing
 
     * high-symmetry points
     * the path sequence
@@ -39,12 +39,6 @@ for i in range(nkpts-1):
     ky = np.linspace(k_points[path[i]][1], k_points[path[i+1]][1], npts)
     kz = np.linspace(k_points[path[i]][2], k_points[path[i+1]][2], npts)
     
-    # remove points that repeat
-    if i != (nkpts-2):
-        kx = np.delete(kx, -1)
-        ky = np.delete(ky, -1)
-        kz = np.delete(kz, -1)
-        
     kxlist = np.hstack((kxlist, kx))
     kylist = np.hstack((kylist, ky))
     kzlist = np.hstack((kzlist, kz))
